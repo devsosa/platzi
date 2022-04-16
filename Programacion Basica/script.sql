649cc51419bf0655e6279b6bfbdf17b56a3d93e7
devsosa <carlos.sosa.c0d3@gmail.com>
2022-04-16 09:19:38 -0600

    Se agrego la carpeta plantilla y cambios en el curso de html, css y terminal linux

---
+++ Programacion Basica/script.sql
@@ -0,0 +1,2 @@
+INSERT INTO lab_suministrante (id, suministrante, activo, codigo_suministrante, id_tipo_conexion, base_url, username, password, ip_equipo, aplicacion, complemento_url, tipo_web_service, tipo_suministrante)
+VALUES (19,'FARLAB - COAGULACION',false,'FARLABLIS','2','http://10.0.66.120:8080','eautomatizado','34ut0m4t1z4d0','10.0.66.120','/MinsalService/wsminsal','/MinsalService/wsminsal','2','1');
---
+++ plantilla_back/app/services/examen.service.js
@@ -0,0 +1,28 @@
+class ExamenesService{
+
+  constructor(){
+    this.users = [];
+  }
+
+  create(){
+
+  }
+
+  find(){
+
+  }
+
+  findOne(){
+
+  }
+
+  update(){
+
+  }
+
+  delete(){
+
+  }
+}
+
+module.exports = ExamenesService;
